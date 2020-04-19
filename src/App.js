1c7d90bab3bba0a4861243960b6325a56406c703
MeyerCode <adam@meyerfam.com>
2020-04-19 17:17:24 +0200

    Broke out a question component that saves the latest guess in the useState hook. Can now render several Question components.

--- src/App.js
+++ src/App.js
@@ -1,46 +1,54 @@
-import React, {useState, useEffect} from 'react';
-var questions = [
+import React, {useState} from 'react';
+import PropTypes from 'prop-types';
+
+const questions = [
   {
-    question: "Vad ås det kan man hur?",
+    content: "Vad ås det kan man hur?",
     answers: [ 	
       { text: "ja", correct: false },
       { text: "nej", correct: false  }, 
       { text: "kanske", correct: true } 
     ]
-  }
+  },
 ]
+
+Question.propTypes = {
+  content: PropTypes.string.isRequired,
+  answers: PropTypes.arrayOf(
+    PropTypes.object  
+  )
+};
+
+function Question(props) {
+  const [guess, setGuess] = useState(null);
+
+  const answers = props.answers.map((answer, index) => 
+    <li key={index} onClick={ () => { 
+        console.log(`You guessed no. ${index}: ${answer.text}`); 
+        setGuess(index); 
+      }}>
+      <span>{answer.text}</span>
+    </li>
+  );
+
+  return(
+    <div className="question-container">
+      <h2><i>{props.content}</i></h2>
+
+      <ul>
+        {answers}
+      </ul>
+
+    </div>
+  );
+}
 
 function App() {
 
-	const [question, setQuestion] = useState(null);
-	useEffect(() => {
-		setQuestion(
-			<div>
-				<h2> {questions[0].question} </h2>
-
-				<form>
-					{console.log(questions[0].answers)}
-					{questions[0].answers.map((a) =>
-						<div className="radio" key={a.text}>
-						<label>
-						<input type="radio"/>
-              {a.text}
-						</label>
-						</div>
-
-					)}
-
-				</form>
-			</div>
-		);
-	},[]);
-
 	return (
 		<div className="App">
-		<h1>Ett quiz</h1>
-		<div className="question">
-		{question}
-		</div>
+      <h1>Ett quiz</h1>
+      <Question content={questions[0].content} answers={questions[0].answers}/>
 		</div>
 	);
 }
